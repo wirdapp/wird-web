b68eeb6cec34e42ff687527d04829ad6b127d6f4
tamimibus <tamimi@Tamimis-MacBook-Pro-2.local>
2024-01-30 08:05:05 +0300

    feat:add reject and add api

--- src/services/members/api.js
+++ src/services/members/api.js
@@ -3,28 +3,41 @@ import { Role } from "../../util/ContestPeople_Role";
 import { getCurrentContestId } from "../contests/utils";
 
 export const MembersApi = {
-  getUsers: async ({ contestId, role, search } = {}) => {
+  getUsers: async ({ contestId, role, search,page_size=10,page=1 } = {}) => {
     const cid = contestId ?? getCurrentContestId();
-    const res = await axios.get(`/admin_panel/${cid}/contest_members/`, {
+    const res = await axios.get(`/admin_panel/bbaa272a-6cb4-4c2c-bb7d-126ef3e7795f/members/`, {
       params: {
-        contest_role: role,
+        contest_role__in: role,
         search,
+        page_size,
+        page
       },
     });
+
     return res.data;
+  
   },
-  getMembers: ({ contestId, search } = {}) =>
-    MembersApi.getUsers({ contestId, role: Role.MEMBER, search }),
+  getAllMemebers: ({ contestId, search,page_size,page } = {}) =>
+  MembersApi.getUsers({ contestId, role: "", search,page_size ,page}),
+  getMembers: ({ contestId, search,page_size,page } = {}) =>
+    MembersApi.getUsers({ contestId, role: Role.MEMBER, search,page_size ,page}),
   getAdmins: ({ contestId, search } = {}) =>
     MembersApi.getUsers({ contestId, role: Role.ADMIN, search }),
   getPending: ({ contestId, search } = {}) =>
     MembersApi.getUsers({ contestId, role: Role.PENDING, search }),
-  getDeactivated: ({ contestId, search } = {}) =>
-    MembersApi.getUsers({ contestId, role: Role.DEACTIVATED, search }),
+  getDeactivated: ({ contestId, search,page_size } = {}) =>
+    MembersApi.getUsers({ contestId, role: Role.DEACTIVATED, search ,page_size}),
   async addUserToContest({ username, contestId, role }) {
     const cid = contestId ?? getCurrentContestId();
-    const res = await axios.post(`/admin_panel/${cid}/contest_people/`, {
+    const res = await axios.post(`/admin_panel/${cid}/members/`, {
       username,
+      contest_role: role,
+    });
+    return res.data;
+  },
+  async approveOrRejectUserToContest({ username, contestId, role }) {
+    const cid = contestId ?? getCurrentContestId();
+    const res = await axios.patch(`/admin_panel/${cid}/members/${username}/`, {
       contest_role: role,
     });
     return res.data;
